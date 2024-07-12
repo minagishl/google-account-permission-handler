d4f1f339910d0fd9109f39cf0efe731d01c051c6
minagishl <mvxd568.co@icloud.com>
2024-07-12 22:29:43 +0900

    Forgot to turn off the console

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,8 +51,6 @@ export default function Home() {
     return url;
   }
 
-  console.log(process.env.NEXT_PUBLIC_REDIRECT_HIDDEN);
-
   return (
     <main className="w-screen h-screen flex justify-center items-center flex-col space-y-3 px-3">
       <form
