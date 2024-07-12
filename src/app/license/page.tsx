8391ce1f552697775b4d55d1b1b52189a04716f5
minagishl <mvxd568.co@icloud.com>
2024-07-12 23:17:03 +0900

    Change display size

--- src/app/license/page.tsx
+++ src/app/license/page.tsx
@@ -14,21 +14,19 @@ export default function License() {
 
   return (
     <Container>
-      <div>
-        <div className="border border-gray-200 p-4 rounded-lg space-y-4">
-          <p className="text-sm font-medium">
-            This website is open source
-            <br />
-            See the GitHub page for licenseinformation
-            <br />
-            <button
-              className="text-blue-600 decoration-2 hover:underline dark:text-blue-500"
-              onClick={handle}
-            >
-              Go to the license
-            </button>
-          </p>
-        </div>
+      <div className="border border-gray-200 p-4 rounded-lg space-y-4 w-full max-w-sm">
+        <p className="text-sm font-medium">
+          This website is open source
+          <br />
+          See the GitHub page for licenseinformation
+          <br />
+          <button
+            className="text-blue-600 decoration-2 hover:underline dark:text-blue-500"
+            onClick={handle}
+          >
+            Go to the license
+          </button>
+        </p>
       </div>
     </Container>
   );
