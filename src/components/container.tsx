8cd94753c4d2714f9df7637aecd39962203b9a06
minagishl <mvxd568.co@icloud.com>
2024-07-13 17:26:36 +0900

    Change space width

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,6 @@
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-center space-y-3 px-3 dark:bg-neutral-900">
+    <main className="flex h-screen w-screen flex-col items-center justify-center space-y-3 px-4 dark:bg-neutral-900">
       {children}
     </main>
   );
