5bf522952aebd9456858fa29c516c341e287c05e
minagishl <mvxd568.co@icloud.com>
2024-07-13 13:47:01 +0900

    Disable at the press of a button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,31 @@
 
 import { useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { tv } from 'tailwind-variants';
+
+// Components
 import Container from '@/components/container';
+
+const button = tv({
+  base: 'inline-flex items-center justify-center gap-x-2 rounded-lg border px-4 py-3 text-sm disabled:pointer-events-none disabled:opacity-50',
+  variants: {
+    color: {
+      primary: 'border-transparent bg-blue-600 text-white hover:bg-blue-700',
+      secondary: 'border-gray-200 bg-white text-gray-800 hover:bg-gray-50',
+    },
+    font: {
+      medium: 'font-medium',
+      semibold: 'font-semibold',
+    },
+  },
+});
 
 const urlPattern = /^https:\/\/.*\.google\.com\/.+$/;
 
 export default function Home() {
   const formUrlRef = useRef<HTMLInputElement>(null);
   const [url, setUrl] = useState('');
+  const [clickButton, setClickButton] = useState<boolean>(false);
   const router = useRouter();
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -28,6 +46,9 @@ export default function Home() {
       alert('The URL must begin with https:// and end with .google.com.');
       return;
     }
+
+    setClickButton(true);
+
     const encodedUrl = encodeURIComponent(transformGoogleFormsUrl(formUrl));
     const newUrl = `https://accounts.google.com/AccountChooser?continue=${encodedUrl}`;
     router.push(newUrl);
@@ -43,6 +64,8 @@ export default function Home() {
       alert('The URL must begin with https:// and end with .google.com.');
       return;
     }
+
+    setClickButton(true);
 
     router.push(modifiedUrl);
   };
@@ -84,7 +107,7 @@ export default function Home() {
               className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer bg-white pl-2"
               onClick={clearUrl}
             >
-              <div className="inline-flex items-center justify-center rounded bg-gray-50 px-3 py-1 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-100 disabled:pointer-events-none disabled:opacity-50">
+              <div className="inline-flex items-center justify-center rounded bg-gray-50 px-3 py-1 text-sm font-medium text-gray-800 hover:bg-gray-100 disabled:pointer-events-none disabled:opacity-50">
                 <p>Clear</p>
               </div>
             </div>
@@ -93,13 +116,21 @@ export default function Home() {
 
         <button
           type="submit"
-          className="inline-flex items-center justify-center gap-x-2 rounded-lg border border-transparent bg-blue-600 px-4 py-3 text-sm font-semibold text-white hover:bg-blue-700 disabled:pointer-events-none disabled:opacity-50"
+          disabled={clickButton}
+          className={button({
+            color: 'primary',
+            font: 'semibold',
+          })}
         >
           Open account switching screen
         </button>
         <button
           type="button"
-          className="inline-flex items-center justify-center gap-x-2 rounded-lg border border-gray-200 bg-white px-4 py-3 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50"
+          disabled={clickButton}
+          className={button({
+            color: 'secondary',
+            font: 'medium',
+          })}
           onClick={handleSecondaryButtonClick}
         >
           Automatically opens in an authorized account
